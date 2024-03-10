f8bba56a3872b4cecbe35bf4472fa3c51f5c32e6
Mark Neil <mark.neil.u.cordero@gmail.com>
2024-03-10 23:39:11 +0800

    adding pseudocode_to_code/1.php

---
+++ php/pseudocode_to_code/1.php
@@ -0,0 +1,26 @@
+<?php
+function findABook(Array $bookList, string $bookName) {
+    $found = FALSE;
+    foreach ($bookList as $index => $book) {
+        if ($book === $bookName) {
+            $found = $index;
+            break;
+        }
+    }
+    return $found;
+}
+
+function placeAllBooks(Array $orderedBooks, Array &$bookList) {
+    foreach ($orderedBooks as $book) {
+        $bookFound = findABook($bookList, $book);
+        if ($bookFound !== FALSE) {
+            array_splice($bookList, $bookFound, 1);
+        }
+    }
+}
+
+$bookList = ['PHP', 'MySQL', 'PGSQL', 'Oracle', 'Java'];
+$orderedBooks = ['MySQL', 'PGSQL', 'Java'];
+placeAllBooks($orderedBooks, $bookList);
+echo implode(',', $bookList);
+?>
